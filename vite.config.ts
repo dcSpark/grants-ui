d4a4078146026a53ee24630a3218d0e459f80d06
Paul Ccari <paulclindo@gmail.com>
2022-07-21 04:10:10 -0500

    setup: npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const path = require("path");
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "lib",
+      fileName: "lib",
+      formats: ["es", "cjs"],
+    },
+  },
   plugins: [react()],
 });
