aa8a0d8ca0f1a8df397480260d0fc5764c894e22
Paul Ccari <paulclindo@gmail.com>
2022-08-12 19:13:21 -0500

    fix: export newsCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as ProposalCard } from "./ProposalCard/ProposalCard";
 export { default as Select } from "./Select/Select";
 export { default as FilterList } from "./FilterList/FilterList";
 export { default as TabGroup } from "./TabGroup/TabGroup";
+export { default as NewsCard } from "./NewsCard/NewsCard";
